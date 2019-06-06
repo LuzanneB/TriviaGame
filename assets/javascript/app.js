091501f564c59095df95ccf05ca84538cf701f99
Luzanne Burt <luzannea@gmail.com>
2019-06-05 21:24:56 -0400

    added questions, possile answers and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,65 @@
 // create a variable score =0
+let score = 0;
 // create a variable correct = 0
+let correct = 0;
 // create a variable incorrect =0
-// create a variable incorrect =0
+let incorrect = 0;
+// create a variable unanswered =0
+let unanswered = 0;
 // create an array with an object for each question, possible answers & answer
+
+let questions = [
+    {
+    question:"What was Al Bundy's Profession",
+    panswers:["Car Salesmen", "Shoe Salesmen", "Doctor", "Accountant"],
+    answer:"Shoe Salesman"
+    },
+    {
+    question:"Who was the actress who played Kelly Bundy?",
+    panswers:["Christina Applegate","Kerri Russel","Elisabeth Shue","Juliette Lewis" ],
+    answer:"Christina Applegate"
+    },
+    {
+    question:"What make of car did Al Bundy drive?",
+    panswers:["Chrysler","Plymouth","Toyota","Dodge"],
+    answer:"Dodge"
+    },
+    {
+    question:"What is Peggy Bundy's maiden name?",
+    panswers:["Wanker", "Smith", "Farmer","Jones"],
+    answer:"Wanker"
+    },
+    {
+    question:"What is Al's nickname for Kelly?",
+    panswers:["Sugar","Pumpkin","Doll","Darling"],
+    answer:"Pumpkin",
+    },
+    {
+    question:"What is 'Bud' Bundy's real first name?",
+    panswers:["Robert", "Buddy", "William", "Buderick"],
+    answer:"Buderick",
+    },
+    {
+    question:"What was the name of the Bundy's first dog?",
+    panswers:["Lucky","Scooter","Buck","Lassie"],
+    answer:"Buck"
+    },
+    {
+    question:"What was the Bundy's female neighbor's name?",
+    panswers:["Darcy","Lucy", "Marcy", "Rhonda"],
+    answer: "Marcy"
+    },
+    {
+    question:"abc",
+    panswers:["1"],
+    answer:"abc"
+    },
+    {
+    question:"def",
+    panswers:["1","2"],
+    answer: "def,"
+    }
+]
 // create a function to pass through the arrays and return coordination question, possible answer and answer
 // create a function (possibly included above) to start a 30 second timer.
 // create a a function so that when start is clicked:
