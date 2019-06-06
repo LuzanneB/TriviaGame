101dc6fa888ed4c751e586f7d6fcbf1de9e9b3fa
Luzanne Burt <luzannea@gmail.com>
2019-06-05 20:21:09 -0400

    psuedo code in js complete

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+// create a variable score =0
+// create a variable correct = 0
+// create a variable incorrect =0
+// create a variable incorrect =0
+// create an array with an object for each question, possible answers & answer
+// create a function to pass through the arrays and return coordination question, possible answer and answer
+// create a function (possibly included above) to start a 30 second timer.
+// create a a function so that when start is clicked:
+    // "start" disapears
+    // 30 second timer appears and starts coundown(function from above)
+    // questions and possible answers appear
+        // if user answers correct, all text disappers and new text appears congrats, with a pic
+        // if user answers incorrect...new text with correct answer and pic
+        // if user does not answer when timer runs out, new text with correct answer and pic
+        // after 5 seconds of the info screen, new question appears
+        // after all questions are answered, new text showing correct, incorrect, unanswered tally appears
+            // also includes "Start Over" that when clicked should reset all the scores and start back at question 1.
+
+
+
+ 
