6564882170ee609939cf605ab73fc55421c41d25
Luzanne Burt <luzannea@gmail.com>
2019-06-06 14:11:44 -0400

    added all images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,54 +12,73 @@ let questions = [
     {
     question:"What was Al Bundy's Profession",
     panswers:["Car Salesmen", "Shoe Salesmen", "Doctor", "Accountant"],
-    answer:"Shoe Salesman"
+    answer:"Shoe Salesman",
+    meme:"assets/images/shoesaleman.jpg",
     },
     {
     question:"Who was the actress who played Kelly Bundy?",
     panswers:["Christina Applegate","Kerri Russel","Elisabeth Shue","Juliette Lewis" ],
-    answer:"Christina Applegate"
+    answer:"Christina Applegate",
+    meme:"assets/images/christinaapplegate.gif"
     },
     {
     question:"What make of car did Al Bundy drive?",
     panswers:["Chrysler","Plymouth","Toyota","Dodge"],
-    answer:"Dodge"
+    answer:"Plymouth",
+    meme:"assets/images/plymouth.jpg",
+
     },
     {
     question:"What is Peggy Bundy's maiden name?",
     panswers:["Wanker", "Smith", "Farmer","Jones"],
-    answer:"Wanker"
+    answer:"Wanker",
+    meme:"assets/images/Peggy.gif",
     },
     {
     question:"What is Al's nickname for Kelly?",
     panswers:["Sugar","Pumpkin","Doll","Darling"],
     answer:"Pumpkin",
+    meme:"assets/images/alandkelly.jpg",
     },
     {
     question:"What is 'Bud' Bundy's real first name?",
     panswers:["Robert", "Buddy", "William", "Buderick"],
     answer:"Buderick",
+    meme:"assets/images/bud.gif",
     },
     {
     question:"What was the name of the Bundy's first dog?",
     panswers:["Lucky","Scooter","Buck","Lassie"],
-    answer:"Buck"
+    answer:"Buck",
+    meme:"assets/images/buck.gif",
     },
     {
     question:"What was the Bundy's female neighbor's name?",
     panswers:["Darcy","Lucy", "Marcy", "Rhonda"],
-    answer: "Marcy"
+    answer: "Marcy",
+    meme:"assets/images/marcy.gif",
     },
     {
     question:"How many seasons did Married with Children run?",
     panswers:["7","11","13","15"],
-    answer:"11"
+    answer:"11",
+    meme:"assets/images/cast.jpg",
     },
     {
     question:"Al Bundy founded what orginazition?",
     panswers:["Yes Ma'am","No Ma'am","Bon Bon Voyage","Bowling Shoe Salesmen"],
     answer: "No Ma'am",
+    meme:"assets/images/nomaam.jpg",
     }
 ]
+
+// var game = {
+//     score:0,
+//     startGame:function(){
+//         $
+//     }
+
+// }
 // create a function to pass through the arrays and return coordination question, possible answer and answer
 // create a function (possibly included above) to start a 30 second timer.
 // create a a function so that when start is clicked:
